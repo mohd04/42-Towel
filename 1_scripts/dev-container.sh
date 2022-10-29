35c2c64a71dd951a4586090e9b4b39c5148aabc6
Abubaker <bu.alseri@live.com>
2022-10-29 20:42:58 +0400

    tester almost done

--- 1_scripts/dev-container.sh
+++ 1_scripts/dev-container.sh
@@ -14,20 +14,20 @@ echo -e "${GREEN}This is a simple installation to download Container\n${NC}"
 echo -e "${GREEN}Is this you personal device or 42 IMAC${NC}"
 echo -e "${GREEN}(1)42 IMAC (2)Personal device ${NC}"
 echo -e "${GREEN}(exit)to do nothing${NC}"
-read answer42
+read answer1
 
-if [ "$answer42" = "exit" ]; then
+if [ "$answer1" = "exit" ]; then
     exit
 fi
 
-if [ "$answer42" = "1" ]; then 
+if [ "$answer1" = "1" ]; then 
     git clone https://github.com/opsec-infosec/42-ValgrindContainer.git
     cd 42-ValgrindContainer
     ./install
     exit
 fi
 
-if [ "$answer42" = "2" ]; then
+if [ "$answer1" = "2" ]; then
     git clone https://github.com/opsec-infosec/42-Devcontainer.git
     exit
 fi
